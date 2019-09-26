519b8efeb39fb82d248e0baa60e73c2aa1c4d1fe
Swen Kooij <swen@sectorlabs.ro>
2019-09-26 20:07:16 +0300

    Empty project set up

---
+++ README.md
@@ -0,0 +1,3 @@
+# py-phone-number-fmt
+
+Google's libphonenumber on steroids. Tries all sorts of crazy combinations in an attempt to create a valid phone number. Useful for those of us who have to deal with poorly sanitized data.
---
+++ setup.py
@@ -0,0 +1,140 @@
+import distutils.cmd
+import os
+import subprocess
+
+from setuptools import find_packages, setup
+
+
+class BaseCommand(distutils.cmd.Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+
+def create_command(text, commands):
+    """Creates a custom setup.py command."""
+
+    class CustomCommand(BaseCommand):
+        description = text
+
+        def run(self):
+            for cmd in commands:
+                subprocess.check_call(cmd)
+
+    return CustomCommand
+
+
+with open(
+    os.path.join(os.path.dirname(__file__), "README.md"), encoding="utf-8"
+) as readme:
+    README = readme.read()
+
+
+setup(
+    name="py-phone-number-fmt",
+    version="1.0b1",
+    packages=find_packages(),
+    include_package_data=True,
+    license="MIT License",
+    description="Google's libphonenumber on steroids.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SectorLabs/py-phone-number-fmt",
+    author="Sector Labs",
+    author_email="open-source@sectorlabs.ro",
+    keywords=["phone number", "phone", "formatting", "validation"],
+    install_requires=["phonenumbers==8.10.13"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+    ],
+    cmdclass={
+        "lint": create_command(
+            "Lints the code",
+            [
+                ["flake8", "setup.py", "phonenumberfmt", "tests"],
+                ["pycodestyle", "setup.py", "phonenumberfmt", "tests"],
+            ],
+        ),
+        "lint_fix": create_command(
+            "Lints the code",
+            [
+                [
+                    "autoflake",
+                    "--remove-all-unused-imports",
+                    "-i",
+                    "-r",
+                    "setup.py",
+                    "phonenumberfmt",
+                    "tests",
+                ],
+                [
+                    "autopep8",
+                    "-i",
+                    "-r",
+                    "setup.py",
+                    "phonenumberfmt",
+                    "tests",
+                ],
+            ],
+        ),
+        "format": create_command(
+            "Formats the code",
+            [["black", "setup.py", "phonenumberfmt", "tests"]],
+        ),
+        "format_verify": create_command(
+            "Checks if the code is auto-formatted",
+            [["black", "--check", "setup.py", "phonenumberfmt", "tests"]],
+        ),
+        "format_docstrings": create_command(
+            "Auto-formats doc strings", [["docformatter", "-r", "-i", "."]]
+        ),
+        "format_docstrings_verify": create_command(
+            "Verifies that doc strings are properly formatted",
+            [["docformatter", "-r", "-c", "."]],
+        ),
+        "sort_imports": create_command(
+            "Automatically sorts imports",
+            [
+                ["isort", "setup.py"],
+                ["isort", "-rc", "phonenumberfmt"],
+                ["isort", "-rc", "tests"],
+            ],
+        ),
+        "sort_imports_verify": create_command(
+            "Verifies all imports are properly sorted.",
+            [
+                ["isort", "-c", "setup.py"],
+                ["isort", "-c", "-rc", "phonenumberfmt"],
+                ["isort", "-c", "-rc", "tests"],
+            ],
+        ),
+        "fix": create_command(
+            "Automatically format code and fix linting errors",
+            [
+                ["python", "setup.py", "format"],
+                ["python", "setup.py", "format_docstrings"],
+                ["python", "setup.py", "sort_imports"],
+                ["python", "setup.py", "lint_fix"],
+            ],
+        ),
+        "verify": create_command(
+            "Verifies whether the code is auto-formatted and has no linting errors",
+            [
+                [
+                    ["python", "setup.py", "format_verify"],
+                    ["python", "setup.py", "format_docstrings_verify"],
+                    ["python", "setup.py", "sort_imports_verify"],
+                    ["python", "setup.py", "lint"],
+                ]
+            ],
+        ),
+    },
+)
