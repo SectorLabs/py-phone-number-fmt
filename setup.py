7a6efcbd2e40a40064ea46786beb4777d0f778a6
Swen Kooij <swen@sectorlabs.ro>
2024-04-03 07:09:20 +0200

    Bump version number to 2.0.0
    Considering we just significantly relaxed the libphonenumber
    requirements, we better release a major version so that
    people are aware that there's a big change.

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ with open(
 
 setup(
     name="py-phone-number-fmt",
-    version="1.2b6",
+    version="2.0.0",
     packages=find_packages(),
     include_package_data=True,
     license="MIT License",
